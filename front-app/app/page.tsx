d776489068a23c89ceb2df43e6742a386d71e4ca
taira7 <0ge3855.0bj121j@gmail.com>
2024-08-26 18:45:52 +0900

    create HomeBar

--- front-app/app/page.tsx
+++ front-app/app/page.tsx
@@ -1,9 +1,11 @@
 import HeaderBar from "./components/HeaderBar";
+import HomeBar from "./components/HomeBar";
 
 export default function Home() {
   return (
     <div>
       <HeaderBar />
+      <HomeBar />
     </div>
   );
 }
