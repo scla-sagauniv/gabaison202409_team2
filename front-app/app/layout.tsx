749968727003c56d454b233e3e9e4d953a26feae
dorapon48 <dorapon48@icloud.com>
2024-08-31 22:45:23 +0900

    update: add prisma, 一部ファイルを移動

--- front-app/app/layout.tsx
+++ front-app/app/layout.tsx
@@ -1,9 +1,8 @@
+import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
-import { getServerSession } from "next-auth";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,13 +17,10 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await getServerSession();
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider>
         {children}
-        </SessionProvider>
       </body>
     </html>
   );
