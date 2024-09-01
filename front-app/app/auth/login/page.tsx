f698b60afa5d88824c5d0be734cbeef81b033a3b
dorapon48 <dorapon48@icloud.com>
2024-09-01 15:00:33 +0900

    fix: conflict

--- front-app/app/auth/login/page.tsx
+++ front-app/app/auth/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-<<<<<<< HEAD
 import { useState } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { redirect } from "next/navigation";
@@ -12,12 +11,10 @@ import * as z from "zod";
 const schema = z.object({
   email: z.string().email("正しいメールアドレスを入力してください"),
 });
-=======
 import BasicButtons from "@/app/components/BasicButtons";
 import UserField from "@/app/components/UserField";
 import User1 from "../../images/user1.png";
 import Image from "next/image";
->>>>>>> main
 
 const Login = () => {
   const { data: session, status } = useSession();
@@ -53,75 +50,6 @@ const Login = () => {
     }
   };
   return (
-<<<<<<< HEAD
-    <>
-      <div className="flex flex-col w-full h-screen text-sm items-center justify-center">
-        <div className="flex flex-col items-center justify-center p-10 border-2 rounded-2xl">
-          <p className="text-2xl font-bold mb-5">ログイン画面</p>
-          <form
-            onSubmit={handleSubmit(handleLogin)}
-            className="flex flex-col items-center"
-          >
-            <div className="text-xs font-bold text-red-400 mb-4">
-              {resError as React.ReactNode}
-            </div>
-            <label htmlFor="email">
-              <p>メールアドレス</p>
-              <input
-                type="text"
-                id="email"
-                {...register("email")}
-                className=" border-2 w-[300px] h-[35px] px-2 mb-2"
-              />
-              <div className="text-xs font-bold text-red-400 mb-2">
-                {errors.email?.message as React.ReactNode}
-              </div>
-            </label>
-            <label htmlFor="password">
-              <p>パスワード</p>
-              <input
-                type="password"
-                id="password"
-                {...register("password")}
-                className=" border-2 w-[300px] h-[35px] px-2 mb-2"
-              />
-              <div className="text-xs font-bold text-red-400 mb-2">
-                {errors.password?.message as React.ReactNode}
-              </div>
-            </label>
-            <button
-              type="submit"
-              className="text-white bg-gray-700 w-[300px] h-[35px] mt-2"
-            >
-              ログイン
-            </button>
-          </form>
-          <hr className="my-4 border-gray-300 w-[300px]" />
-          <div className="flex flex-col items-center">
-            <button
-              onClick={() => {
-                signIn("github");
-              }}
-              className="bg-white text-black border-2 w-[300px] h-[35px] mb-2"
-            >
-              Githubでログイン
-            </button>
-            <button
-              onClick={() => {
-                signIn("google");
-              }}
-              className="bg-white text-black border-2 w-[300px] h-[35px] mb-2"
-            >
-              Googleでログイン
-            </button>
-            <Link href="/signup" className="mt-2">
-              新規登録はこちら
-            </Link>
-          </div>
-        </div>
-      </div>
-    </>
-=======
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 px-4 overflow-x-hidden">
       <div className="mb-10 text-center">
         <Image src={User1} alt="User" width={100} height={100} />
@@ -153,7 +81,6 @@ const Login = () => {
         </ul>
       </nav>
     </div>
->>>>>>> main
   );
 };
 
