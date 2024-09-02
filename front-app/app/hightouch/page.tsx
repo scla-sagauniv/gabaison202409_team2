7bd8a291e1da3393ce1ce16c3515ec589bc1e21c
dorapon48 <dorapon48@icloud.com>
2024-09-03 00:04:03 +0900

    delete: 必要な部分に凝縮

--- front-app/app/hightouch/page.tsx
+++ front-app/app/hightouch/page.tsx
@@ -2,7 +2,6 @@
 import { useState, useEffect } from 'react';
 
 const DeviceMotion = () => {
-  const [permissionGranted, setPermissionGranted] = useState<boolean>(false);
   const [acceleration, setAcceleration] = useState({ x: 0, y: 0, z: 0 });
   const [teststate, setTeststate] = useState("test");
 
@@ -12,7 +11,6 @@ const DeviceMotion = () => {
         const state: PermissionState = await (DeviceMotionEvent as any).requestPermission();
         setTeststate(state);
         if (state === 'granted') {
-          setPermissionGranted(true);
           setDeviceMotionEvent();
         } else {
           alert('動作と方向へのアクセスを許可してください');
@@ -21,7 +19,6 @@ const DeviceMotion = () => {
         console.error('Permission request error:', error);
       }
     } else {
-      setPermissionGranted(true);
       setDeviceMotionEvent();
     }
   };
@@ -39,58 +36,14 @@ const DeviceMotion = () => {
   };
 
   return (
-    <div data-index={permissionGranted ? '1' : '0'}>
-      {!permissionGranted ? (
-        <div className="box">
-          <button id="btn" onClick={requestPermission}>START</button>
-        </div>
-      ) : (
-        <div className="box">
-          <div>
-            <p id="x">x: {acceleration.x.toFixed(2)}</p>
-            <p id="y">y: {acceleration.y.toFixed(2)}</p>
-            <p id="z">z: {acceleration.z.toFixed(2)}</p>
-          </div>
-        </div>
-      )}
-      <div>
-        {teststate}
-        <p id="x">x: {acceleration.x.toFixed(2)}</p>
-        <p id="y">y: {acceleration.y.toFixed(2)}</p>
-        <p id="z">z: {acceleration.z.toFixed(2)}</p>
+    <div>
+      <div className="box">
+        <button id="btn" onClick={requestPermission}>START</button>
       </div>
-      <style jsx>{`
-        .box {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          position: fixed;
-          top: 0;
-          bottom: 0;
-          left: 0;
-          right: 0;
-        }
-
-        [data-index='0'] .box:nth-child(2) {
-          display: none;
-        }
-
-        [data-index='1'] .box:nth-child(1) {
-          display: none;
-        }
-
-        #x:before {
-          content: 'x: ';
-        }
-
-        #y:before {
-          content: 'y: ';
-        }
-
-        #z:before {
-          content: 'z: ';
-        }
-      `}</style>
+      {teststate}
+      <p id="x">x: {acceleration.x.toFixed(2)}</p>
+      <p id="y">y: {acceleration.y.toFixed(2)}</p>
+      <p id="z">z: {acceleration.z.toFixed(2)}</p>
     </div>
   );
 };
