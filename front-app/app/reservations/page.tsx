176ce9b515970e09f2c990d05a36d11cd7e97e0b
Yuki01232 <yukimaeda0220@gmail.com>
2024-09-02 14:33:23 +0900

    a

--- front-app/app/reservations/page.tsx
+++ front-app/app/reservations/page.tsx
@@ -9,14 +9,15 @@ const Reservation = () => {
   return (
     <div>
       {/* <HeaderBar /> */}
-      <div className="text-2xl font-bold text-center pt-12">予約詳細画面</div>
+      <div className="text-2xl font-bold text-center pt-12">参加確認画面</div>
       <div className="flex justify-center ">
-        <Image src={Restaurant} alt="User" width={500} height={500} />
+        <Image src={Restaurant} alt="User" width={500} height={500} className="w-full h-auto max-w-xs mx-auto"/>
       </div>
       <div className="font-bold text-left">焼肉キング</div>
       <div>お店の紹介</div>
-      <div className="py-5">お店の位置情報</div>
-      <div className="h-64 bg-blue-500 border border-blue-700 rounded-lg shadow-lg w-full"></div>
+      <div className="flex justfy-center w-96 h-32 border border-black "></div>
+      <div className="pt-5">お店の位置情報</div>
+      <div className="h-32 w-64 border border-black rounded-lg shadow-lg"></div>
       <div>現在の人数</div>
 
       {/* <HomeBar /> */}
